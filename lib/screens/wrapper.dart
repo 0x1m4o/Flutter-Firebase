c2237aaee0610ff8afe42bda3dc2392c4fd2d0aa
0x1m4o <chuntdevnx@gmail.com>
2023-03-27 11:30:46 +0700

    Firebase Set Up and Basic App Structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../screens/home/home.dart';
+import '../screens/auth/authentication.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authentication();
+  }
+}
