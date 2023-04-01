aaa8c8568567786d774fcad5c815c6d8c056cd7b
0x1m4o <chuntdevnx@gmail.com>
2023-04-02 03:16:37 +0700

    Using a Stream Builder

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/user.dart';
+import 'package:flutter_firebase/services/database.dart';
+import 'package:flutter_firebase/shared/loading.dart';
+import 'package:provider/provider.dart';
 import '../../shared/text_fields_constant.dart';
 
 class DialogContent extends StatefulWidget {
@@ -17,115 +21,130 @@ class _DialogContentState extends State<DialogContent> {
   int? _currentStrength;
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Update Your Profile',
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) =>
-                  value!.isEmpty ? 'Please Enter a Name' : null,
-              decoration: textInputDecoration.copyWith(
-                hintText: 'Your Username',
-                labelText: 'Username',
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _currentName = value;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              child: DropdownButtonFormField(
-                value: _currentSugars ?? '0',
-                isExpanded: true,
-                decoration: textInputDecoration,
-                items: sugars.map((sugar) {
-                  return DropdownMenuItem(
-                    child: Container(
+    final user = Provider.of<MyUser?>(context);
+    return StreamBuilder<UserData?>(
+        stream: DatabaseService(uid: user!.uid).userSnap,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData? userData = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Update Your Profile',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      initialValue: userData!.name,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Please Enter a Name' : null,
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Your Username',
+                        labelText: 'Username',
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _currentName = value;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
                       width: double.infinity,
-                      child: Text('$sugar Sugars'),
+                      child: DropdownButtonFormField(
+                        value: _currentSugars ?? userData.sugars,
+                        isExpanded: true,
+                        decoration: textInputDecoration,
+                        items: sugars.map((sugar) {
+                          return DropdownMenuItem(
+                            child: Container(
+                              width: double.infinity,
+                              child: Text('$sugar Sugars'),
+                            ),
+                            value: sugar,
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _currentSugars = value;
+                          });
+                        },
+                      ),
                     ),
-                    value: sugar,
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _currentSugars = value;
-                  });
-                },
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Slider(
+                      value:
+                          (_currentStrength ?? userData.strength)!.toDouble(),
+                      onChanged: (value) => setState(() {
+                        _currentStrength = value.round();
+                      }),
+                      min: 100,
+                      max: 900,
+                      divisions: 8,
+                      activeColor:
+                          Colors.brown[_currentStrength ?? userData.strength!],
+                      inactiveColor:
+                          Colors.brown[_currentStrength ?? userData.strength!],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          width: 90,
+                          height: 40,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Close'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red[400],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        SizedBox(
+                          width: 90,
+                          height: 40,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              print(_currentName);
+                              print(_currentSugars);
+                              print(_currentStrength);
+                            },
+                            child: Text('Save'),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue[600]),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Slider(
-              value: (_currentStrength ?? 100).toDouble(),
-              onChanged: (value) => setState(() {
-                _currentStrength = value.round();
-              }),
-              min: 100,
-              max: 900,
-              divisions: 8,
-              activeColor: Colors.brown[_currentStrength ?? 100],
-              inactiveColor: Colors.brown[_currentStrength ?? 100 ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                  width: 90,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text('Close'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red[400],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 90,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      print(_currentName);
-                      print(_currentSugars);
-                      print(_currentStrength);
-                    },
-                    child: Text('Save'),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[600]),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase/models/user.dart';
 import '../models/data_model.dart';
 
 class DatabaseService {
   /// Collection Reference
 
-  final String uid;
-  DatabaseService({required this.uid});
+  final String? uid;
+  DatabaseService({this.uid});
   final CollectionReference dataCollection =
       FirebaseFirestore.instance.collection('dataCollection');
 
@@ -32,5 +33,21 @@ class DatabaseService {
         sugars: documents['sugars'] ?? '',
       );
     }).toList();
+  }
+
+  // Get user doc Stream
+  Stream<UserData?> get userSnap {
+    return dataCollection.doc(uid).snapshots().map(_userdataFromSnapshots);
+  }
+
+  UserData? _userdataFromSnapshots(DocumentSnapshot snapshot) {
+    Map<String, dynamic> data = snapshot.data()! as Map<String, dynamic>;
+
+    return UserData(
+      uid: data['uid'],
+      name: data['name'],
+      sugars: data['sugars'],
+      strength: data['strength'],
+    );
   }
 }
